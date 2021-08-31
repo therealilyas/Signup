7c20a60e0d0a622cd9474561ae0c5c2bfc21f69b
Ilyas <ilyas.sultanov2020@list.ru>
2021-08-31 19:48:01 +0500

    console.log deleted

--- js/event.js
+++ js/event.js
@@ -3,17 +3,14 @@ import SERVICE from './service.js';
 
 
 const signup = DOM.signUpBtn.addEventListener('click', () => {
-    console.log("sign up")
     SERVICE.signUp()
 })
 
 const signin = DOM.signInBtn.addEventListener('click', () => {
-    console.log("sign in")
     SERVICE.signIn()
 })
 
 const signout = DOM.signOutBtn.addEventListener('click', () => {
-    console.log("sign out")
     SERVICE.signOut()
 })
 
